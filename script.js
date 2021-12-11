e92b3195cbac10ad7a436f56a6fb4a89b49a0e82
loch <nigelloch@gmail.com>
2021-12-11 14:13:57 -0600

    added for loop to color hour containers

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+// remember to convert hours id's in html
 
 
+// iterate through each hour to see which is in the past, present and future
 function  workDaySchedule() {
 
+    var textArea = document.getElementsByTagName("textArea")
     // current hour
     var currHour = moment().hours();
-    console.log(currHour)
+   // console.log(currHour)
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
+    for (let i = 0; i < textArea.length; i++) {
+        const element = textArea[i];
         
+        var past = "rgb(255, 60, 60)"
+        var present = "white"
+        var future = "rgb(139, 139, 255"
+
+        //console.log(element.getAttribute("id"))
+        var textAreaHour = element.getAttribute("id")
+
+        if (textAreaHour < currHour) {
+            element.style.background = past;
+        }
+        else if (textAreaHour == currHour) {
+            element.style.background = present;
+        }
+        else {
+            element.style.background = future;
+        }
     }
 
-var 
-
-
 }
+
+workDaySchedule();
